d7617e0ef29b9d953bcf88db60b4ade702b2b57e
Matiimov <mattimov@gmail.com>
2025-04-23 10:58:49 +1000

    Initial Commit

---
+++ BubblePop/BubblePopApp.swift
@@ -0,0 +1,17 @@
+//
+//  BubblePopApp.swift
+//  BubblePop
+//
+//  Created by Mati on 4/23/25.
+//
+
+import SwiftUI
+
+@main
+struct BubblePopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

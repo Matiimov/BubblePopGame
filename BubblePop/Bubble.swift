09f556718ee7c4d6513195391b9d7c4293a72e10
Matiimov <mattimov@gmail.com>
2025-04-25 15:16:11 +1000

    Updated version of bubble.swift

--- BubblePop/Bubble.swift
+++ BubblePop/Bubble.swift
@@ -1,8 +1,14 @@
 import SwiftUI
 
-enum BubbleColorType: CaseIterable {
-    case red, pink, green, blue, black
+/// **Bubble.swift**
+/// Defines the BubbleColorType and Bubble structures used in the game.
+/// BubbleColorType encapsulates scoring, display color, spawn probabilities per color, and time bonuses.
 
+/// Represents all possible bubble colors and their associated behaviors
+enum BubbleColorType: CaseIterable {
+    case red, pink, green, blue, black, gold
+
+    /// **Base points** awarded when popping this bubble
     var points: Int {
         switch self {
         case .red:   return 1
@@ -10,19 +16,19 @@ enum BubbleColorType: CaseIterable {
         case .green: return 5
         case .blue:  return 8
         case .black: return 10
+        case .gold:  return 0 /// Gold bubbles give no points (time bonus instead)
         }
     }
 
-    var probability: Double {
+    /// **Bonus time** awarded only when popping a gold bubble
+    var timeBonus: Int {
         switch self {
-        case .red:   return 0.40
-        case .pink:  return 0.30
-        case .green: return 0.15
-        case .blue:  return 0.10
-        case .black: return 0.05
+        case .gold: return 10 /// Only gold bubbles grant extra time
+        default:    return 0 /// Other colors grant no time bonus
         }
     }
 
+    /// **Display color** Render the bubble with the right color on screen
     var color: Color {
         switch self {
         case .red:   return .red
@@ -30,12 +36,35 @@ enum BubbleColorType: CaseIterable {
         case .green: return .green
         case .blue:  return .blue
         case .black: return .black
+        case .gold:
+              return Color(
+                  red: 212/255,
+                  green: 175/255,
+                  blue: 55/255
+              ) /// Custom gold RGB for special bubbles
+          }
+    }
+
+    /// **Standard bubble types** (excludes gold)
+    static let normalCases: [BubbleColorType] = [.red, .pink, .green, .blue, .black]
+
+    /// **Spawn probability** for each normal bubble
+    var probability: Double {
+        switch self {
+        case .red:   return 0.40
+        case .pink:  return 0.30
+        case .green: return 0.15
+        case .blue:  return 0.10
+        case .black: return 0.05
+        case .gold:  return 0 /// Gold is handled separately with a fixed chance
         }
     }
 }
 
+/// Represents a single bubble instance in the game
 struct Bubble: Identifiable {
-    let id = UUID()
+    let id = UUID() /// Unique identifier for this bubble
     let type: BubbleColorType
-    var position: CGPoint
+    var position: CGPoint /// Current position in the play area
+    var direction: CGVector /// Direction unit vector indicating bubble movement
 }
